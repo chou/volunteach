22051656f1f8455a84640e504f96f6a7b7a6e5e4
Sam Balinghasay <samuel.balinghasay@gmail.com>
2013-10-05 11:57:42 -0700

    finish users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,16 +12,15 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  # def update
-
-  #   @user = User.find(params[:user_id])
-  #   User.transaction do
-  #     @user.update_attributes(params[:user])
-
-
-  #   end
-  # end
+  def update
+    @user = User.find(params[:user_id])
+    @user.update_attributes(params[:user])
+    @user.topic_ids = params[:user][:topic_ids]
+  end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render true, status: :ok
   end
 end
