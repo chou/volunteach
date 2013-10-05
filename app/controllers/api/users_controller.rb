96924bcc87072e10df1dde54069cb039222a17db
Sam Balinghasay <samuel.balinghasay@gmail.com>
2013-10-05 10:25:14 -0700

    Added RABL to topics, users, meetings.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
   end
 
   def create
--- app/views/api/users/show.rabl
+++ app/views/api/users/show.rabl
@@ -1,1 +1,2 @@
 object @user
+attributes *(User.column_names - ["session_token", "password_digest"])
