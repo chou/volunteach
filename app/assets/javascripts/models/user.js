14a1e788ebb8798fdb859c5c2c331102fa190208
Sam Balinghasay <samuel.balinghasay@gmail.com>
2013-10-05 22:49:42 -0700

    add name helpers on User.js model

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,12 @@
 TuberApp.Models.User = Backbone.Model.extend({
-  urlRoot : '/api/users'
+  urlRoot : '/api/users',
+
+  fullName: function() {
+    return this.get('fname') + " " + this.get('lname');
+  },
+
+  abbrevName: function() {
+    return this.get('fname') + " " + this.get('lname').slice(0, 1);
+  }
+
 });
